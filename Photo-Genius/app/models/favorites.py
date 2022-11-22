c84fe44ca0b3c4803f24307bec0f6ad98a417b32
Trevor <Trevor1798@yahoo.com>
2022-11-22 16:18:31 -0500

    finished building models, now going to create db relationships

---
+++ Photo-Genius/app/models/favorites.py
@@ -0,0 +1,19 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+
+class Favorite(db.Model):
+    __tablename__ = 'favorites'
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, primary_key=True)
+    image_id = db.Column(db.Integer, primary_key=True)
+
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'user_id': self.user_id,
+            'image_id': self.image_id
+        }
--- Photo-Genius/app/models/tags.py
+++ Photo-Genius/app/models/tags.py
@@ -1,22 +1,20 @@
 from .db import db, environment, SCHEMA, add_prefix_for_prod
 
-class Comment(db.Model):
-    __tablename__ = 'comments'
+
+class Tag(db.Model):
+    __tablenme__ = 'tags'
 
     if environment == "production":
         __table_args__ = {'schema': SCHEMA}
-        
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id'), nullable=False))
-    description = db.Column(db.String(1000), nullable=False)
-    image_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('images.id'), nullable=False))
 
+    id = db.Column(db.Integer, primary_key=True)
+    image_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('images.id'), nullable=False))
+    tag_name = db.Column(db.String(75), nullable=False)
 
 
     def to_dict(self):
         return {
             'id': self.id,
-            'body': self.body,
-            'user_id': self.user_id,
-            'image_id': self.image_id
+            'image_id': self.image_id,
+            'tag_name': self.tag_name
         }
