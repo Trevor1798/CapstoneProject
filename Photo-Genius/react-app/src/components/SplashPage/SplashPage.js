71f33d0e02c535ff97e630553d1e37640ff7884c
Trevor <Trevor1798@yahoo.com>
2022-11-23 18:19:58 -0500

    working on styling for splash page

--- Photo-Genius/react-app/src/components/SplashPage/SplashPage.js
+++ Photo-Genius/react-app/src/components/SplashPage/SplashPage.js
@@ -6,21 +6,32 @@ const SplashPage = () => {
     let history = useHistory()
 
     return (
-        <div className='splashpage wrapper'>
+        <div className='splashpage-wrapper'>
+
             <div className='splash-nav'>
-                <div className='photo-genius-logo'></div>
-                <div className='search-bar'></div>
-                <div className='splash-login-new' onClick={() => history.push('/signup')}>Sign Up</div>
+
+                <div className='splash-top-left'>
+                <div className='splash-logo-header' onClick={() => history.push('/')}>
+                <div className='photo-genius-logo1'>•</div>
+                <div className='photo-genius-logo2'>•</div>
+                <div className='photo-genius'>photo-genius</div>
+                </div>
+                </div>
+
+
+                <div className='logins'>
                 <div className='splash-login' onClick={() => history.push('/login')}>Log In</div>
+                <div className='splash-login-new' onClick={() => history.push('/signup')}>Sign Up</div>
+                </div>
             </div>
 
         <div className='splashpage'>
-            <img className="splashpage-image" src="https://c4.wallpaperflare.com/wallpaper/998/700/928/anime-art-boat-night-water-wallpaper-preview.jpg"></img>
+            <img className="splashpage-image" src="https://www.pixel4k.com/wp-content/uploads/2018/09/night-city-city-lights-metropolis-night-4k_1538067561.jpg"></img>
             <div className='splash-welcome'>Find your inspiration</div>
             <div className='splash-text'>Join the Photo-Genius community, home to tens of billions of photos and 2 million groups</div>
             <div className='start-for-free' onClick={() => history.push('/signup')}>Start for free</div>
-        </div>
 
+        </div>
         </div>
     )
 }
