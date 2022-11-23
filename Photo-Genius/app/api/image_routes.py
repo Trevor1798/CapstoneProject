035c00d47d9ca2bd072e55b76e8c19a579c0c02f
Trevor <Trevor1798@yahoo.com>
2022-11-22 19:22:31 -0500

    tested all 4 routes on images, everything is working

--- Photo-Genius/app/api/image_routes.py
+++ Photo-Genius/app/api/image_routes.py
@@ -9,7 +9,9 @@ image_routes = Blueprint('images', __name__)
 @image_routes.route('/')
 def get_all_images():
     all_images = Image.query.all()
-    return all_images.to_dict()
+    get_images = []
+    get_images.extend([i.to_dict() for i in all_images])
+    return {"images": get_images}
 
 
 # Create an Image
@@ -17,23 +19,22 @@ def get_all_images():
 @login_required
 def create_image():
     form = ImageForm()
-    user_id = current_user.id
     form['csrf_token'].data = request.cookies['csrf_token']
     if form.validate_on_submit():
         params = {
             "title": form.data["title"],
             "description": form.data["description"],
             "image_url": form.data["image_url"],
-            "user_id": current_user.id
+            # "user_id": current_user.id
         }
         new_image = Image(**params)
         db.session.add(new_image)
         db.session.commit()
-        return new_image.to_dict()
+        return {"images": new_image.to_dict()}
 
 
 # Update an Image
-@image_routes.route('/<int:id>')
+@image_routes.route('/<int:id>', methods=["PUT"])
 @login_required
 def edit_image(id):
     form = ImageForm()
@@ -49,11 +50,10 @@ def edit_image(id):
 
 
 # Delete an Image
-@image_routes.route('/<int:id>')
-@login_required
+@image_routes.route('/<int:id>', methods=["DELETE"])
+# @login_required
 def delete_image(id):
     image = Image.query.get(id)
     db.session.delete(image)
     db.session.commit()
     return {"message": "Image Deleted"}
-    
