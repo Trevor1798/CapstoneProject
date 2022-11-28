4b5f75fa1996b7c6c5ef3cc0616927492f881f09
Trevor <Trevor1798@yahoo.com>
2022-11-27 22:57:33 -0500

    test-render

--- migrations/versions/5c07e45b683e_.py
+++ migrations/versions/5c07e45b683e_.py
@@ -1,14 +1,16 @@
 """empty message
 
 Revision ID: 5c07e45b683e
-Revises: 
+Revises:
 Create Date: 2022-11-27 22:55:34.665723
 
 """
 from alembic import op
 import sqlalchemy as sa
 
-
+import os
+environment = os.getenv("FLASK_ENV")
+SCHEMA = os.environ.get("SCHEMA")
 # revision identifiers, used by Alembic.
 revision = '5c07e45b683e'
 down_revision = None
