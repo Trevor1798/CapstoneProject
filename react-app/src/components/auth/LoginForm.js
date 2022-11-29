e90a6567e2a9141e73c4a7124a5f661a104e7b2b
Trevor <Trevor1798@yahoo.com>
2022-11-29 18:29:19 -0500

    working on form styling finished login-form working on error validations for login now

--- react-app/src/components/auth/LoginForm.js
+++ react-app/src/components/auth/LoginForm.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { Redirect } from 'react-router-dom';
+import { Redirect, useHistory } from 'react-router-dom';
 import { login } from '../../store/session';
 import './LoginForm.css'
 
 const LoginForm = () => {
+  let history = useHistory()
   const [errors, setErrors] = useState([]);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -33,10 +34,15 @@ const LoginForm = () => {
 
   return (
     <div className='login-wrapper'>
-      <img className='background-image' src='https://live.staticflickr.com/3829/14240294606_8a157ddfc2_b.jpg'></img>
 
+    <img className='background-image' src='https://live.staticflickr.com/3829/14240294606_8a157ddfc2_b.jpg'></img>
     <div className='login-container'>
-    <form onSubmit={onLogin}>
+    <form class='login-form' onSubmit={onLogin}>
+      <div className='logo'>
+      <div className='login-genius-logo1'>•</div>
+      <div className='login-genius-logo2'>•</div>
+      </div>
+      <div className='login-welcome'>Log in to Photo-Genius</div>
       <div classname='error-wrapper'>
         {errors.map((error, ind) => (
           <div key={ind}>{error}</div>
@@ -59,7 +65,14 @@ const LoginForm = () => {
           value={password}
           onChange={updatePassword}
           />
-        <button type='submit'>Login</button>
+        <button className='login-button' type='submit'>Login</button>
+      </div>
+      <div className='demo-user'>
+        <button className='demo' type="submit" onClick={() => {setEmail('demo@aa.io'); setPassword('password');}}>Demo User</button>
+      </div>
+      <div className='signup-wrapper'>
+        <div className='signup-welcome'>Not a member yet?</div>
+        <div className='redirect' onClick={() => history.push('/sign-up')}>Sign up here</div>
       </div>
     </form>
     </div>
